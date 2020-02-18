c275287668d9869442a26ddd43849a7452792bd8
JUNGEEYOU <junge2u@naver.com>
2020-02-18 22:30:32 +0900

    포트폴리오 리스트 만들기 및 메뉴바 쪼개기

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,15 @@
 
 var express = require('express');
 var router = express.Router();
+const fs = require('fs');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index');
+    console.log("ttt");
+  let rawdata = fs.readFileSync('./public/list.json');
+  let myport = JSON.parse(rawdata);
+  console.log("tetst", myport);
+  res.render('index', {portlist:myport});
 });
 
 module.exports = router;
