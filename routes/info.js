83dd91d15b7af74bbf3891a87d2ffd000f969a7c
lena <junge2u@naver.com>
2020-02-19 17:39:29 +0900

    pre page add

--- routes/info.js
+++ routes/info.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
+const fs = require('fs');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('info', { title: 'Express' });
+  console.log("ttt");
+  let rawdata = fs.readFileSync('./public/list.json');
+  let myport = JSON.parse(rawdata);
+  console.log("my info ", myport);
+  res.render('info', {portlist:myport});
 });
 
 module.exports = router;
--- routes/pre.js
+++ routes/pre.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
+const fs = require('fs');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('pre', { title: 'Express' });
+  let rawdata = fs.readFileSync('./public/list.json');
+  let myport = JSON.parse(rawdata);
+  console.log("my pre ", myport);
+  res.render('pre', {portlist:myport});
 });
 
 module.exports = router;
