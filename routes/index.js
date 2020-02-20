502ad4ff3da4349d27bc772f0a50d3f4f0759f91
JUNGEEYOU <junge2u@naver.com>
2020-02-20 21:47:12 +0900

    전체 재 수정

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,10 @@
 
 var express = require('express');
 var router = express.Router();
-const fs = require('fs');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    console.log("ttt");
-  let rawdata = fs.readFileSync('./public/list.json');
-  let myport = JSON.parse(rawdata);
-  console.log("tetst", myport);
-  res.render('index', {portlist:myport});
+  res.render('index',);
 });
 
 module.exports = router;
