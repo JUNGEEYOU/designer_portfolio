79f421ec054dd3e320e09d1721e73b8319a0df51
JUNGEEYOU <junge2u@naver.com>
2020-06-03 13:25:59 +0900

    수정

--- routes/projects.js
+++ routes/projects.js
@@ -1,13 +1,32 @@
 var express = require('express');
 var router = express.Router();
-const fs = require('fs');
+// const fs = require('fs');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  let rawdata = fs.readFileSync('./public/list.json');
-  let myport = JSON.parse(rawdata);
-  console.log("my pre ", myport);
-  res.render('projects', {portlist:myport});
+  res.render('projects', {});
+});
+
+router.get('/1', function(req, res, next) {
+  res.render('projects/1', {});
+});
+
+router.get('/2', function(req, res, next) {
+  res.render('projects/2', {});
+});
+
+router.get('/3', function(req, res, next) {
+  res.render('projects/3', {});
+});
+router.get('/4', function(req, res, next) {
+  res.render('projects/4', {});
+});
+router.get('/5', function(req, res, next) {
+  res.render('projects/5', {});
+});
+
+router.get('/6', function(req, res, next) {
+  res.render('projects/6', {});
 });
 
 module.exports = router;
---
+++ routes/projects_2.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+const fs = require('fs');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  let rawdata = fs.readFileSync('./public/list.json');
+  let myport = JSON.parse(rawdata);
+  console.log("my pre ", myport);
+  res.render('projects', {portlist:myport});
+});
+
+module.exports = router;
