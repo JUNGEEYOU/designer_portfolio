02ccae0b7b25b9d1e92dbf4ff6be46321104ebe8
lena <junge2u@naver.com>
2020-06-26 11:42:23 +0900

    추가 수정 - 사진 옆으로 넘기기, project 대문자, 프로필 사진 줄이기

--- routes/other.js
+++ routes/other.js
@@ -2,12 +2,19 @@ var express = require('express');
 var router = express.Router();
 const fs = require('fs');
 
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  console.log("ttt");
-  let rawdata = fs.readFileSync('./public/list.json');
-  let myport = JSON.parse(rawdata);
-  res.render('other', {portlist:myport});
+  res.render('other', {});
 });
+
+router.get('/1', function(req, res, next) {
+  res.render('other/1', {});
+});
+
+router.get('/2', function(req, res, next) {
+  res.render('other/2', {});
+});
+
 
 module.exports = router; 
